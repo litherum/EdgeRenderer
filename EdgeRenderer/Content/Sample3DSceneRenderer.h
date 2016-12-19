2003326c17e2bca4bda5aa9fd4b07ad38af257e8
Litherum <litherum@gmail.com>
2016-12-19 13:56:41 -0800

    Geometry Shader

--- EdgeRenderer/Content/Sample3DSceneRenderer.h
+++ EdgeRenderer/Content/Sample3DSceneRenderer.h
@@ -47,6 +47,7 @@ namespace EdgeRenderer
 		UINT												m_cbvDescriptorSize;
 		D3D12_RECT											m_scissorRect;
 		std::vector<byte>									m_vertexShader;
+		std::vector<byte>									m_geometryShader;
 		std::vector<byte>									m_pixelShader;
 		D3D12_VERTEX_BUFFER_VIEW							m_vertexBufferView;
 		//D3D12_INDEX_BUFFER_VIEW								m_indexBufferView;
--- EdgeRenderer/Content/SampleVertexShader.hlsl
+++ EdgeRenderer/Content/SampleVertexShader.hlsl
@@ -14,7 +14,7 @@ struct VertexShaderInput
 };
 
 // Per-pixel color data passed through the pixel shader.
-struct PixelShaderInput
+struct GeometryShaderInput
 {
 	float4 pos : SV_POSITION;
 	float3 color : COLOR0;
@@ -22,9 +22,9 @@ struct PixelShaderInput
 };
 
 // Simple shader to do vertex processing on the GPU.
-PixelShaderInput main(VertexShaderInput input)
+GeometryShaderInput main(VertexShaderInput input)
 {
-	PixelShaderInput output;
+	GeometryShaderInput output;
 	float4 pos = float4(input.pos, 1.0f);
 
 	// Transform the vertex position into projected space.
